5e1caafef8885a30d73351edc7b7096df3b588f2
Robert <robertdominiccalinescu@gmail.com>
2026-03-17 13:21:52 -0600

    Updated app.js with sessions, Passport, availability of logged-in user in every EJS template

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
-
 const session = require("express-session");
-const passport = require("passport");
 require("dotenv").config();
 
+const passport = require("passport");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/public")));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
@@ -20,23 +23,20 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "../frontend/public")));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
-// EJS setup
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../frontend/views"));
 
-// Temporary home route for testing
 app.get("/", (req, res) => {
   res.send("UR Involved backend is running.");
 });
 
-// Auth routes
 app.use("/", authRoutes);
 
-// MongoDB connection
 mongoose
   .connect("mongodb://127.0.0.1:27017/ur_involved")
   .then(() => {
