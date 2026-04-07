b5c5e1e7b6f24bfe065b75f4296c203e499dae3d
Robert <robertdominiccalinescu@gmail.com>
2026-04-07 13:27:47 -0600

    debugging

--- SourceCode/backend/controllers/clubController.js
+++ SourceCode/backend/controllers/clubController.js
@@ -5,19 +5,27 @@ const mongoose = require("mongoose");
 
 exports.showHomePage = async (req, res) => {
   try {
-    const clubs = await Club.find({ approved: true }).sort({ name: 1 });
-    let tempUsername = "";
-    if (await req.user) {
-      tempUsername = req.user.email;
-    }
-    const username = tempUsername;
+    const { clubs, search, sort, filter } = await getClubsData(req.query);
+
     res.render("home", { 
       clubs,
-      username});
+      currentSearch: search,
+      currentSort: sort,
+      currentFilter: filter
+    });
+
   } catch (error) {
     console.error("failure rendering home page", error);
     res.status(500).send("Error loading homepage.");
   }
+};
+
+exports.showEditProfile = async (req, res) => {
+  res.render("editProfile", { user: req.user });
+};
+
+exports.showClubCreation = async (req, res) => {
+  res.render("createClub", { user: req.user });
 };
 
 // used to update club list on homepage without a full page reload
@@ -141,20 +149,12 @@ exports.showDashboard = async (req, res) => {
   }
 };
 
-exports.showEditProfile = async (req, res) => {
-  res.render("editProfile", {user: req.user});
-}
-
-exports.showClubCreation = async (req, res) => {
-  res.render("createClub", {user: req.user});
-}
-
 exports.submitJoinRequest = async (req, res) => {
   try {
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
       return res.status(400).send("Invalid club ID.");
     }
-    
+
     const club = await Club.findOne({
       _id: req.params.id,
       approved: true
@@ -438,4 +438,3 @@ exports.rejectJoinRequest = async (req, res) => {
     res.status(500).send("Error rejecting join request.");
   }
 };
-
